aa721fc92bc5b4a264d94f0dc4bb5d881b54ed4d
Rom <96356930+RomFR57@users.noreply.github.com>
2021-12-23 11:56:28 +0100

    basic benchmark added

---
+++ benchmark.py
@@ -0,0 +1,44 @@
+from time import time_ns
+import numpy as np
+import fastfinance as ff
+
+
+def benchmark(data=None):
+    print("$$$ FASTFINANCE BENCHMARK $$$")
+    init = np.random.uniform(low=1, high=1000, size=10)
+    if data is None:
+        data = np.random.uniform(low=1, high=1000, size=int(input("DATA SIZE : ")))
+    size = len(data)
+    benchmark_list = [
+        ["INIT", ff.sma, (init, 1)],
+        ["SMA", ff.sma, (data, size)],
+        ["INIT", ff.ema, (init, 1)],
+        ["EMA", ff.ema, (data, size)],
+        ["INIT", ff.dema, (init, 1)],
+        ["DEMA", ff.dema, (data, size)],
+        ["INIT", ff.trix, (init, 1)],
+        ["TRIX", ff.trix, (data, size)],
+        ["INIT", ff.macd, (init, 1, 1)],
+        ["MACD", ff.macd, (data, size, size)],
+        ["INIT", ff.stochastic, (init, 2, 1)],
+        ["STOCHASTIC", ff.stochastic, (data, size, size)],
+        ["INIT", ff.rsi, (init, 1)],
+        ["RSI", ff.rsi, (data, size)],
+        ["INIT", ff.srsi, (init, 5)],
+        ["S-RSI", ff.srsi, (data, size)],
+        ["INIT", ff.bollinger_bands, (init, 1)],
+        ["BOLLINGER BANDS", ff.bollinger_bands, (data, size)],
+        ["INIT", ff.heiken_ashi, (init, init, init, init)],
+        ["HEIKEN ASHI", ff.heiken_ashi, (data, data, data, data)],
+        ["INIT", ff.ichimoku, (init, 1, 1, 1, 1)],
+        ["ICHIMOKU", ff.ichimoku, [data]],
+        ["INIT", ff.volume_profile, (init, init, 1)],
+        ["VOLUME PROFILE", ff.volume_profile, (data, data, size)],
+    ]
+    for i, p in enumerate(benchmark_list):
+        if p[0] == "INIT":
+            p[1](*p[2])
+        else:
+            t = time_ns()
+            p[1](*p[2])
+            print("".join([p[0], " [", str(round((time_ns() - t) / 1000000)), " ms]"]))
