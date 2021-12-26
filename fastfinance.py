volume profile normalization fix

fastfinance.py
@@ -28,16 +28,6 @@ def np_clip(a, a_min, a_max, out=None):
 
 
 @jit(nopython=True)
-def normalize(data):
-    """
-    Normalize
-    :type data: np.ndarray
-    :rtype: np.ndarray
-    """
-    return data / np.linalg.norm(data)
-
-
-@jit(nopython=True)
 def convolve(data, kernel):
     """
     Convolution 1D Array
@@ -431,7 +421,9 @@ def volume_profile(c_close, c_volume, bins=10):
     sum_h = np.array([0.0] * bins)
     for i in range(len(c_close)):
         sum_h[int((c_close[i] - min_close) * bins * norm)] += c_volume[i] ** 2
-    return normalize(np.sqrt(sum_h)), np.linspace(min_close, max_close, bins)
+    sq = np.sqrt(sum_h)
+    print(sum(sq / sum(sq)))
+    return sq / sum(sq), np.linspace(min_close, max_close, bins)
 
 
 @jit(nopython=True)